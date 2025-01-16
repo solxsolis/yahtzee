deeccc903e03644dc9800b08eb2fd2bab66fa4df
atamelo <anna.tamelo@gmail.com>
2025-01-16 20:35:01 +0100

    added player class

---
+++ game/player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self, name, score=0):
+        self.name = name
+        self.score = score
+
+    def get_name(self):
+        return self.name
+
+    def get_score(self):
+        return self.score
+
