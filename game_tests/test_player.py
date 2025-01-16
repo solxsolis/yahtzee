f077e6e946f855d7bb848e3e33c82fe435974ccd
atamelo <anna.tamelo@gmail.com>
2025-01-16 20:38:34 +0100

    small fix

--- game_tests/test_player.py
+++ game_tests/test_player.py
@@ -4,6 +4,6 @@ from game.player import Player
 def test_init():
     player1 = Player("Player1")
     player2 = Player("Player2", 100)
-    assert player1.name == "Player1"
-    assert player2.name == "Player2"
-    assert player2.score == 100
+    assert player1.get_name() == "Player1"
+    assert player2.get_name() == "Player2"
+    assert player2.get_score() == 100
