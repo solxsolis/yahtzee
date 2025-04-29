9921b3bb2b79eaea1d804fe6acb097c0727b66cd
anna-tamelo <anna.tamelo@gmail.com>
2025-04-29 17:23:49 +0200

    added id argument

--- connection/client_main.py
+++ connection/client_main.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument("--host", default = "127.0.0.1")
     p.add_argument("--port", type = int, default = 9999)
+    p.add_argument("--id", type = int, choises = [0,1], required = True, help = "Your player ID: 0 or 1")
     args = p.parse_args()
 
-    app = YahtzeeGUI(mode="network", host=args.host, port=args.port)
+    app = YahtzeeGUI(mode="join", host=args.host, port=args.port, player_id = args.id)
     app.mainloop()
